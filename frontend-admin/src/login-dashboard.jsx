d3ba1e3ac7cc82f8d0be54187ba44ee0416410b7
IbrahimAliAbel <ibrahimaliabel019@gmail.com>
2025-08-15 22:25:42 +0700

    updatelogin

--- frontend-admin/src/login-dashboard.jsx
+++ frontend-admin/src/login-dashboard.jsx
@@ -218,7 +218,7 @@ export default function LoginPage() {
                 </div>
               ) : (
                 <>
-                  Sign In
+                  Sign In ok
                   <ArrowRight className="ml-2" size={20} />
                 </>
               )}
